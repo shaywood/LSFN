e9907a3db11eaaada3864c826444aceb582254f8
Glenn Searby <glenn.searby@gmail.com>
2012-07-03 22:45:53 +0100

    Made GUI centre on our ship. Coloured our ship green, and other ships red.

--- src/com/wikispaces/lsfn/Interface/Display2D/MapDisplay.java
+++ src/com/wikispaces/lsfn/Interface/Display2D/MapDisplay.java
@@ -3,15 +3,20 @@ package com.wikispaces.lsfn.Interface.Display2D;
 import com.wikispaces.lsfn.Interface.Model.*;
 
 import java.awt.Dimension;
+import java.awt.geom.Point2D;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial") // not intending to serialize UI elements
 public class MapDisplay extends JFrame {
+	int width = 1024;
+	int height = 1024;
+	
 	public MapDisplay(KnownSpace world) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setMinimumSize(new Dimension(1024, 1024));
+		this.setSize(width, height);
 		getRootPane().setDoubleBuffered(true);
-		getContentPane().add(new SpacePanel(world));
+		getContentPane().add(new SpacePanel(world, new Point2D.Double(width/2, height/2)));
 		setVisible(true);
 	}
 	
